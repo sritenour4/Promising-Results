5a71234837a5b9f3e0c9fa423fc5cbb84a3bb88b
Shannon Ritenour <shannonritenour@gmail.com>
2021-02-23 11:12:20 -0600

    added a comment

--- promising-results-master/app.js
+++ promising-results-master/app.js
@@ -31,23 +31,34 @@ slowMath.add(6, 2)
     console.log(err);
 });
 
+// .catch(err => {throw err})
+
 // Async/Await
 
 let doMath = async () => { 
     try {
         let result = await slowMath.add(6, 2);
+        console.log(result);
         result = await slowMath.divide(result, 4);
+        console.log(result);
         result = await slowMath.multiply(result, 2);
+        console.log(result);
         result = await slowMath.subtract(result, 3);
+        console.log(result);
         result = await slowMath.add(result, 98);
+        console.log(result);
         result = await slowMath.remainder(result, 2);
+        console.log(result);
         result = await slowMath.multiply(result, 50);
+        console.log(result);
         result = await slowMath.remainder(result, 40);
+        console.log(result);
         result = await slowMath.add(result, 32);
+        console.log(result);
         return console.log(`The final result is ${result}.`);        
         
     } catch (err) {
-        console.log(err);
+        console.error(err);
     }
 }
 
